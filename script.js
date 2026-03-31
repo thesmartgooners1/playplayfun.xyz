943f799fecabc74a59ab7b5e3fe9804b0bb61214
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-03-31 14:42:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const matches = [
+const fetch = require("node-fetch");
+
+fetch("https://ultimate-football-data-api.p.rapidapi.com/ufd/21/players", {
+  headers: {
+    "x-rapidapi-key": "YOUR_API_KEY",
+    "x-rapidapi-host": "ultimate-football-data-api.p.rapidapi.com"
+  }
+})
+.then(res => res.json())
+.then(data => console.log(data))
   {
     home: "Manchester United",
     away: "Liverpool",
